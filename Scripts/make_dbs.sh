b386c498fa224964ab661f05b84afaf3510479c3
Brandon Bizzarro <bbbizzarro@gmail.com>
2022-08-18 16:40:17 -0700

    Format new states json-csv database for states database implementation.

--- Scripts/make_dbs.sh
+++ Scripts/make_dbs.sh
@@ -2,3 +2,4 @@
 
 python3 ./Scripts/CSV_to_JSON.py caravaner_tile_db.txt caravaner_tile_db.json
 python3 ./Scripts/CSV_to_JSON.py caravaner_icon_db.txt caravaner_icon_db.json
+python3 ./Scripts/CSV_to_JSON.py caravaner_states_db.txt caravaner_states_db.json
